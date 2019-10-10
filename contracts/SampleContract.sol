29c0dc12987a644e745930db2d585ada5b943bd0
liping <283005921@qq.com>
2019-10-11 01:01:55 +0800

    test

--- contracts/SampleContract.sol
+++ contracts/SampleContract.sol
@@ -1,13 +1,5 @@
-pragma solidity ^0.4.4;
-
-contract SampleContract {
-
-    // This value is visible in etherscan.io explorer
-    uint256 public value;
-
-    // Anyone can call this contract and override the value of the previous caller
-    function setValue(uint256 value_) public {
-        value = value_;
+contract C {
+    function f() public returns (uint) {
+        return 88888888;
     }
-
 }
